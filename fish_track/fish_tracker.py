56b77450ed982cb683ec8617d12997904dce7c46
YaoDongcai <1207542617@qq.com>
2024-09-17 11:36:06 +0800

    feat: add fist track page

---
+++ fish_track/fish_tracker.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import glob
+
+# Load an official or custom model
+# model = YOLO('yolov8n.pt')  # Load an official Detect model
+model = YOLO('./best.pt')  # Load an official Detect model
+# model = YOLO('yolov8n-seg.pt')  # Load an official Segment model
+# model = YOLO('yolov8n-pose.pt')  # Load an official Pose model
+# model = YOLO('path/to/best.pt')  # Load a custom trained model
+
+# Perform tracking with the model
+# results = model.track(source="https://youtu.be/Zgi9g1ksQHc", show=True)  # Tracking with default tracker
+# results = model.track(source="https://youtu.be/Zgi9g1ksQHc", show=True, tracker="bytetrack.yaml")  # Tracking with ByteTrack tracker
+# results = model.track(source="/home/q_hu/workspace6/MOT/ultralytics/videos/10-fish-part2-2016_raw.mp4", show=False, tracker="bytetrack.yaml", save_txt=True, save=True, save_conf=True)  # Tracking with ByteTrack tracker
+
+files = glob.glob("./images/*.JPG")
+print(files)
+for path in files:
+    results = model.track(source=path, show=False, tracker="bytetrack.yaml", stream=True, save_txt=True, save=True, save_conf=True)  # Tracking with ByteTrack tracker
+    for r in results:
+        pass
+# print(results)
