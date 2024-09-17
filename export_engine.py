eff411e3d4645981f1bcdc202cae3a9800716b76
YaoDongcai <1207542617@qq.com>
2024-09-17 17:04:33 +0800

    modify: 修改模型

--- export_engine.py
+++ export_engine.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a YOLOv8n PyTorch model
-model = YOLO("yolov8n.pt")
+model = YOLO("best.pt")
 
 # Export the model
 # model.export(format="engine")  # creates 'yolov8n.engine'
