435a880853aa612da025216be36b9eb7df2398b2
YaoDongcai <1207542617@qq.com>
2024-09-17 16:58:30 +0800

    feat: modify fish track context

--- fish_tracker.py
+++ fish_tracker.py
@@ -3,7 +3,7 @@ import glob
 
 # Load an official or custom model
 # model = YOLO('yolov8n.pt')  # Load an official Detect model
-model = YOLO('best.pt')  # Load an official Detect model
+model = YOLO('./best.pt')  # Load an official Detect model
 # model = YOLO('yolov8n-seg.pt')  # Load an official Segment model
 # model = YOLO('yolov8n-pose.pt')  # Load an official Pose model
 # model = YOLO('path/to/best.pt')  # Load a custom trained model
